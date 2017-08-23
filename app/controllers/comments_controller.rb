b04f806b6cf43ff6bab504b3a44bdca6f657c8fd
Kelli Parris <kelli@hsdesignhouse.com>
2017-08-23 14:41:09 -0500

    action cable...possibly working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,8 @@ class CommentsController < ApplicationController
     @comment = @recipe.comments.build(comment_params)
     @comment.chef = current_chef
     if @comment.save
-      flash[:success] = "Comment was created successfully"
-      redirect_to recipe_path(@recipe)
+      ActionCable.server.broadcast "comments",
+                  render(partial: 'comments/comment', object: @comment)
     else
       flash[:danger] = "Comment was not created"
       redirect_to :back
