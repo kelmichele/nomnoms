70bc61a90250a7a39762913064f249b84c60cfbe
Kelli Parris <kelli@hsdesignhouse.com>
2017-08-23 14:48:54 -0500

    make app ready for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://snacks-on-snacks.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://snacks-on-snacks.herokuapp.com', 'http://snacks-on-snacks.herokuapp.com']
 end
