5fbf01b6d9f9995e48627db88a9c434ee4100252
Kelli Parris <kelli@hsdesignhouse.com>
2017-08-28 16:54:50 -0500

    images for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
 	validates :name, presence: true
-	validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+	validates :description, presence: true, length: { minimum: 5, maximum: 5000 }
 	validates :chef_id, presence: true
 	belongs_to :chef
 	default_scope -> { order(updated_at: :desc) }
@@ -8,6 +8,7 @@ class Recipe < ApplicationRecord
 	has_many :ingredients, through: :recipe_ingredients
 	has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  mount_uploader :image, ImageUploader
 
   def thumbs_up_total
 	  self.likes.where(like: true).size
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824212859) do
+ActiveRecord::Schema.define(version: 20170828160928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170824212859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chef_id"
+    t.string "image"
   end
 
 end
